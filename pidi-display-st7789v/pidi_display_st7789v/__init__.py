97cae53f1caf96ef47d4642b523777698f15724c
jeremie-gabolde <jeremie-gabolde@outlook.fr>
2025-11-17 20:11:19 +0000

    added st7789v support

---
+++ pidi-display-st7789v/pidi_display_st7789v/__init__.py
@@ -0,0 +1,43 @@
+"""ST7789V Display plugin for PiDi (Mopidy)."""
+import RPi.GPIO as GPIO
+from st7789v.interface import RaspberryPi
+from st7789v import Display
+from pidi_display_pil import DisplayPIL
+
+__version__ = "0.1.0"
+
+
+class DisplayST7789V(DisplayPIL):
+    """PiDi display output plugin for the ST7798V 1.3" 320x240 SPI LCD."""
+
+    option_name = "st7789v"
+
+    def __init__(self, args):
+        super().__init__(args)
+
+        # Set GPIO numbering mode
+        GPIO.setmode(GPIO.BCM)  # use BCM numbering (GPIO numbers, not pins)
+
+        # Open SPI interface
+        self._rpi = RaspberryPi()
+        self._rpi.open()
+
+        # Initialize display
+        self._st7789v = Display(self._rpi)
+        self._st7789v.initialize(rotation=args.rotation or 0)
+
+    def start(self):
+        """Called when PiDi starts."""
+        # Optionally, turn backlight fully on
+        self._st7789v.set_backlight(100)
+
+    def stop(self):
+        """Called when PiDi stops."""
+        self._st7789v.set_backlight
+
+    def redraw(self):
+        """Draw the current Pillow image to the ST7789V."""
+        if DisplayPIL.redraw(self):
+            # Convert Pillow image to RGB bytes
+            data = list(self._output_image.convert("RGB").getdata())
+            self._st7789v.draw_rgb_bytes(data)
